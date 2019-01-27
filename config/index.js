906bbc8af8e868b626c5323b7c17c5f7bbff9279
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 11:22:32 -0500

    AH Star Wars Final App 2

---
+++ config/index.js
@@ -0,0 +1,3 @@
+const MONGOLAB_IVORY_URI = 'mongodb://heroku_zfl6h505:l7t7itk9rhokdrpl04r65knsl8@ds113855.mlab.com:13855/heroku_zfl6h505';
+
+module.exports = MONGOLAB_IVORY_URI;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 var cors = require('cors');
+const URI = require('../config/index');
 
 
-mongoose.connect('mongodb://localhost/planets')
+// mongoose.connect('mongodb://localhost/planets')
+mongoose.connect(process.env.MONGOLAB_IVORY_URI || URI);
 
 const app = express()
 

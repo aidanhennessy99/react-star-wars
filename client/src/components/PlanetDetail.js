af60384350981f88586b037b8a3b4216e2b2508b
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 19:06:39 -0500

    AH Star Wars Final Draft 20

--- client/src/components/PlanetDetail.js
+++ client/src/components/PlanetDetail.js
@@ -24,7 +24,7 @@ const PlanetDetail = ({props, planets, addToTravelList}) => {
   return (
       <div className="details">
     {/* An image, the name, climate, population, terrain, diameter and surface water all appear when we click on the planet link to view details.      */}
-    <div class="card w-25">
+    <div class="card w-50">
         <img src = {baseURI + planet.name + endURI} alt="img" class="card-img-top" />       
         <h5 className="card-text">Name: {planet.name}</h5>   
         <h5 className="card-text">Climate: {planet.climate}</h5>

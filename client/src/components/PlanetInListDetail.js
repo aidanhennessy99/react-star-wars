4ee1d820f78421f5b389210e3c6286f3c5703e3c
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 23:57:22 -0500

    AH Star Wars 101

--- client/src/components/PlanetInListDetail.js
+++ client/src/components/PlanetInListDetail.js
@@ -25,11 +25,11 @@ const PlanetInListDetail = ({props, travelList}) => {
       <div className="details-of-List text-center"> 
              <h1>Planet Details</h1>
       </div>
-     
+      <div className="row">
         <div className="card-columns d-flex justify-content-center">
        
           <div className="card">
-          <div className="row justify-content-md-center">
+       
          
       {/* //The details that come back just like in the PlanetDetail component are an image, the name, climate, population, terrain, diameter and surface water of the planet. The big difference from PlaentDetail is that it does not have a button where you can add a new planet.    */}
            <img src = {baseURI + TravelListPlanet.name + endURI} alt="img" class="card-img-top"/>

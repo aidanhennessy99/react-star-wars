5d305cee9c29730f48b1a19b3e7ad912e69d1024
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 23:57:22 -0500

    AH Draft 80

--- client/src/components/PlanetInListDetail.js
+++ client/src/components/PlanetInListDetail.js
@@ -25,7 +25,7 @@ const PlanetInListDetail = ({props, travelList}) => {
              <h1>Planet Details</h1>
       </div>
       <div class="row justify-content-md-center">
-        <div className="card-columns d-flex justify-content-center">
+        <div className="card-columns">
           <div className="card">
         
          

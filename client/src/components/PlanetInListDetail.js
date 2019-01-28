054b97960e982a4cd62818a12278ec6caca64e75
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 23:57:22 -0500

    AH Star Wars 97

--- client/src/components/PlanetInListDetail.js
+++ client/src/components/PlanetInListDetail.js
@@ -24,7 +24,7 @@ const PlanetInListDetail = ({props, travelList}) => {
       <div className="details-of-List text-center"> 
              <h1>Planet Details</h1>
       </div>
-      <div className="row justify-content-lg-center">
+      <div className="row justify-content-sm-center">
         <div className="card-columns d-flex justify-content-center">
        
           <div className="card">

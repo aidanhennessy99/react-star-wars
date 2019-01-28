50847a510c78b1e415f17c9d91e3d51e24c81ce6
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 21:48:03 -0500

    AH Star Wars Draft 33

--- client/src/components/PlanetDetail.js
+++ client/src/components/PlanetDetail.js
@@ -22,7 +22,7 @@ const PlanetDetail = ({props, planets, addToTravelList}) => {
     props.history.push('/');
   }
   return (
-      <div className="details">
+      <div className="card-columns">
     {/* An image, the name, climate, population, terrain, diameter and surface water all appear when we click on the planet link to view details.      */}
     <div class="card">
         <img src = {baseURI + planet.name + endURI} alt="img" class="card-img-top" />       

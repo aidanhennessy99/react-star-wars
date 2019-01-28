e518bb272fcc56cb444e89f8a1cfbc3d7ed3067d
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 23:57:22 -0500

    AH Final Star Wars Project Submission

--- client/src/components/PlanetDetail.js
+++ client/src/components/PlanetDetail.js
@@ -37,6 +37,7 @@ const PlanetDetail = ({props, planets, addToTravelList}) => {
        {/* Handle Submit Click is going to be needed in this constant.  */}
        {/* This is the button that posts the planet to your travelList and your backend database. */}
         <button type="button" onClick={handleClick}>Add Planet to Travel List</button>
+        <br />
         <Link to='/'>Main Menu</Link>
 
         </div>

a74de03f2d60db499283766a2b4db09651ce51d2
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 23:57:22 -0500

    AH Draft 71

--- client/src/components/PlanetInListDetail.js
+++ client/src/components/PlanetInListDetail.js
@@ -29,7 +29,7 @@ const PlanetInListDetail = ({props, travelList}) => {
         
          
       {/* //The details that come back just like in the PlanetDetail component are an image, the name, climate, population, terrain, diameter and surface water of the planet. The big difference from PlaentDetail is that it does not have a button where you can add a new planet.    */}
-           <img src = {baseURI + TravelListPlanet.name + endURI} alt="img" class="card-img-top" max-width="1500" max-height="1500"/>
+           <img src = {baseURI + TravelListPlanet.name + endURI} alt="img" class="card-img-top" max-width="500" max-height="500"/>
            
         <h5 className="card-text">Name: {TravelListPlanet.name}</h5>    
         <h5 className="card-text">Climate: {TravelListPlanet.climate}</h5>

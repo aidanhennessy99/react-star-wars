708f35647ba459bfd17bc3908b62bc2a0dddca4e
aidanhennessy99 <aidanhennessy99@gmail.com>
2019-01-27 19:28:26 -0500

    AH Star Wars 25

--- client/src/components/Planets.js
+++ client/src/components/Planets.js
@@ -21,7 +21,7 @@ class Planets extends React.Component {
             this.props.planets.map(p => (
               <li key={p.name} className="results">
               
-              <div className="card w-100">
+              <div className="card w-100 p-2 bd-highlight">
                   
                   <Link to={`/${p.name}`}><img src={baseURI + p.name + endURI} class="card-img-top"/></Link>
               <h6 className="card-title"><Link to={`/${p.name}`}>{p.name}</Link></h6>    
